47b62b057b5c4e3b5715751dadb8230d03500a3e
deitz88 <jason_deitz_72@yahoo.com>
2021-09-17 19:01:50 -0700

    smaller

--- nebula_generator.py
+++ nebula_generator.py
@@ -1,150 +1,150 @@
   
 import cairo, sys, argparse, copy, math, random
 
-float_gen = lambda a, b: random.uniform(a, b)
+def make_nebula():
 
-colors = []
-for x in range(10):
-    colors.append((0,0,0))
-for i in range(15):
-    colors.append((float_gen(.1, 1), float_gen(.1, .8), float_gen(.1, 1)))
-    colors.append((0, 0, 0))
+    float_gen = lambda a, b: random.uniform(a, b)
 
-def draw_background(cr, width, height):
-    cr.rectangle(0, 0, width, height)
-    cr.set_source_rgb(0, 0, 0)
-    cr.fill()
+    colors = []
+    # for x in range(10):
+    #     colors.append((0,0,0))
+    for i in range(15):
+        colors.append((float_gen(.1, 1.3), float_gen(.1, .5), float_gen(.1, 1.3)))
+        colors.append((0, 0, 0))
 
-def octagon(x_orig, y_orig, side):
-    x = x_orig
-    y = y_orig
-    d = side / math.sqrt(2)
+    def octagon(x_orig, y_orig, side):
+        side = side * random.randint(1, 3)
+        x = x_orig
+        y = y_orig
+        d = side / math.sqrt(2)
 
-    oct = []
+        oct = []
 
-    oct.append((x, y))
+        oct.append((x, y))
 
-    x += side
-    oct.append((x, y))
+        x += side
+        oct.append((x, y))
 
-    x += d
-    y += d
-    oct.append((x, y))
+        x += d
+        y += d
+        oct.append((x, y))
 
-    y += side
-    oct.append((x, y))
+        y += side
+        oct.append((x, y))
 
-    x -= d
-    y += d
-    oct.append((x, y))
+        x -= d
+        y += d
+        oct.append((x, y))
 
-    x -= side
-    oct.append((x, y))
+        x -= side
+        oct.append((x, y))
 
-    x -= d
-    y -= d
-    oct.append((x, y))
+        x -= d
+        y -= d
+        oct.append((x, y))
 
-    y -= side
-    oct.append((x, y))
+        y -= side
+        oct.append((x, y))
 
-    x += d
-    y -= d
-    oct.append((x, y))
+        x += d
+        y -= d
+        oct.append((x, y))
 
-    return oct
+        return oct
 
-def deform(shape, iterations, variance):
-    for i in range(iterations):
-        for j in range(len(shape)-1, 0, -1):
-            midpoint = ((shape[j-1][0] + shape[j][0])/2 + float_gen(-variance, variance), (shape[j-1][1] + shape[j][1])/2 + float_gen(-variance, variance))
-            shape.insert(j, midpoint)
-    return shape
+    def deform(shape, iterations, variance):
+        for i in range(iterations):
+            for j in range(len(shape)-1, 0, -1):
+                midpoint = ((shape[j-1][0] + shape[j][0])/2 + float_gen(-variance, variance), (shape[j-1][1] + shape[j][1])/2 + float_gen(-variance, variance))
+                shape.insert(j, midpoint)
+        return shape
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--width", default=3000, type=int)
-    parser.add_argument("--height", default=2000, type=int)
-    parser.add_argument("-i", "--initial", default=120, type=int)
-    parser.add_argument("-d", "--deviation", default=1500, type=int)
-    parser.add_argument("-bd", "--basedeforms", default=1, type=int)
-    parser.add_argument("-fd", "--finaldeforms", default=3, type=int)
-    parser.add_argument("-mins", "--minshapes", default=1, type=int)
-    parser.add_argument("-maxs", "--maxshapes", default=10, type=int)
-    parser.add_argument("-sa", "--shapealpha", default=.0085, type=float)
-    args = parser.parse_args()
+    def main():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--width", default=3000, type=int)
+        parser.add_argument("--height", default=2000, type=int)
+        parser.add_argument("-i", "--initial", default=1, type=int)
+        parser.add_argument("-d", "--deviation", default=500, type=int)
+        parser.add_argument("-bd", "--basedeforms", default=1, type=int)
+        parser.add_argument("-fd", "--finaldeforms", default=3, type=int)
+        parser.add_argument("-mins", "--minshapes", default=1, type=int)
+        parser.add_argument("-maxs", "--maxshapes", default=200, type=int)
+        parser.add_argument("-sa", "--shapealpha", default=.0045, type=float)
+        # parser.add_argument("--mult", default=random.randint(1, 5), type=range)
+        args = parser.parse_args()
 
-    width, height = args.width, args.height
-    initial = args.initial
-    deviation = args.deviation
+        width, height = args.width, args.height
+        initial = args.initial
+        deviation = args.deviation
 
-    basedeforms = args.basedeforms
-    finaldeforms = args.finaldeforms
+        basedeforms = args.basedeforms
+        finaldeforms = args.finaldeforms
 
-    minshapes = args.minshapes
-    maxshapes = args.maxshapes
-
-    shapealpha = args. shapealpha
-
-    ims = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
-    cr = cairo.Context(ims)
-
-    cr.set_source_rgb(.9, .9, .9)
-    cr.rectangle(0, 0, width, height)
-    cr.fill()
-    
-    draw_background(cr, width, height)
-
-    cr.set_line_width(1)
-
-    for p in range(-int(height*.2), int(height*1.2), 60):
-        cr.set_source_rgba(random.choice(colors)[0], random.choice(colors)[1], random.choice(colors)[2], shapealpha)
-
-        shape = octagon(random.randint(-100, width+100), p, random.randint(100, 300))
-        baseshape = deform(shape, basedeforms, initial)
-
-        for j in range(random.randint(minshapes, maxshapes)):
-        # for j in range(1, 5):
-            tempshape = copy.deepcopy(baseshape)
-            layer = deform(tempshape, finaldeforms, deviation)
-
-            for i in range(len(layer)):
-                cr.line_to(layer[i][0], layer[i][1])
-            cr.fill()
-
-    for p in range(-int(height*.2), int(height*1.2), 60):
-        cr.set_source_rgba(0, 0, 0, shapealpha)
-
-        shape = octagon(random.randint(-100, width+100), p, random.randint(100, 300))
-        baseshape = deform(shape, basedeforms, initial)
-
-        # for j in range(random.randint(minshapes, maxshapes)):
-        for j in range(20):
-            tempshape = copy.deepcopy(baseshape)
-            layer = deform(tempshape, finaldeforms, deviation)
-
-            for i in range(len(layer)):
-                cr.line_to(layer[i][0], layer[i][1])
-            cr.fill()
+        minshapes = args.minshapes
+        maxshapes = args.maxshapes
+        # mult = args.mult
+        
+        # if mult == 1:
+        #     multi = (random.randint(width, width+500), 1.9)
+        # elif mult == 2:
+        #     multi = (1.5, 9)
+        # elif mult == 3:
+        #     multi = (.9, 1.2)
+        # elif mult == 4:
+        #     multi = (.4, .8)
             
-    # add black here?
-    for p in range(-int(height*.2), int(height*1.2), 60):
-        cr.set_source_rgba(0, 0, 0, shapealpha)
+        shapealpha = args.shapealpha
 
-        shape = octagon(random.randint(-100, width+100), p, random.randint(100, 300))
-        baseshape = deform(shape, 0, 0)
+        ims = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
+        cr = cairo.Context(ims)
 
-        # for j in range(random.randint(minshapes, maxshapes)):
-        for j in range(2):
-            tempshape = copy.deepcopy(baseshape)
-            layer = deform(tempshape, finaldeforms, deviation)
+        # background black
+        cr.set_source_rgb(0, 0, 0)
+        # cr.rectangle(0, 0, width*3, height*3)
+        cr.rectangle(0, 0, width, height)
+        
+        cr.fill()
+        
 
-            for i in range(len(layer)):
-                cr.line_to(layer[i][0], layer[i][1])
-            cr.fill()
-    
-    ims.write_to_png('Examples/nebula' + '.png')
+        cr.set_line_width(1)
 
-if __name__ == "__main__":
-    main()
+        for p in range(-int((height*.2)/3), int((height*1.2)/3), 80):
+            cr.set_source_rgba(random.choice(colors)[0], random.choice(colors)[1], random.choice(colors)[2], shapealpha)
+
+            # shape = octagon(random.randint(-100, width+100), p, random.randint(100, 300))0
+            # shape = octagon(random.randint(width*.9, width*1.4), p, random.randint(100, 300))
+            shape = octagon(random.randint(width*.3, width*.8), random.randint(1, height), random.randint(4, 30))
+            
+            baseshape = deform(shape, basedeforms, initial)
+
+            for j in range(random.randint(minshapes, maxshapes)):
+            # for j in range(1, 5):
+                tempshape = copy.deepcopy(baseshape)
+                layer = deform(tempshape, finaldeforms, deviation)
+
+                for i in range(len(layer)):
+                    cr.line_to(layer[i][0], layer[i][1])
+                cr.fill()
+
+        # for p in range(-int(height*.2), int(height*1.2), 60):
+        #     cr.set_source_rgba(0, 0, 0, 2)
+
+        #     shape = octagon(random.randint(-100, width+100), (random.randint(2.3*height, 2.9*height)), random.randint(100, 300))
+        #     baseshape = deform(shape, basedeforms, initial)
+
+        #     # for j in range(random.randint(minshapes, maxshapes)):
+        #     for j in range(20):
+        #         tempshape = copy.deepcopy(baseshape)
+        #         layer = deform(tempshape, finaldeforms, deviation)
+
+        #         for i in range(len(layer)):
+        #             cr.line_to(layer[i][0], layer[i][1])
+        #         cr.fill()
+
+        
+        ims.write_to_png('Examples/nebula' + '.png')
+
+    if __name__ == "__main__":
+        main()
+make_nebula()
